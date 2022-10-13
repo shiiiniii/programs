af01bab181a2b55099af8bc1d9fa80670f7f1fc6
Anastasia <nastya-shishkina-1999@mail.ru>
2022-10-13 17:41:25 +0300

    add task1

---
+++ task1/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+Console.WriteLine("Является ли Ваше первое число квадратом второго?");
+Console.WriteLine("Введите первое число, которое хотите проверить ");
+int numberA = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число, которое хотите проверить ");
+int numberB = int.Parse(Console.ReadLine());
+int sqr = numberB * numberB;
+if (sqr == numberA)
+{
+Console.WriteLine("Да");
+}
+else
+{
+Console.WriteLine("Нет");
+}
