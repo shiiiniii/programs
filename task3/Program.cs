ff8a1d8a245071a9a935ce60dae3939e0ee0862b
Anastasia <nastya-shishkina-1999@mail.ru>
2022-10-13 17:41:45 +0300

    add task3

---
+++ task3/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число ");
+int number = int.Parse(Console.ReadLine());
+int kostyl = number / 10;
+int kray = number - kostyl * 10;
+Console.WriteLine($"Последняя цифра числа {number} равна {kray}");
