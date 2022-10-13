4c565816ddb61b02df94c0a217ae820786b94444
Anastasia <nastya-shishkina-1999@mail.ru>
2022-10-13 17:41:56 +0300

    add task4

---
+++ task4/Program.cs
@@ -0,0 +1,7 @@
+﻿// Напишите программу, которая на вход принимает число и выдаёт его квадрат.
+Console.Clear();
+Console.Write("Введите квадрат какого числа Вы хотите получить: ");
+int number = int.Parse(Console.ReadLine());
+//Находим квадрат числа
+int sqr = number * number;
+Console.WriteLine($"Квадрат числа {number} равен {sqr}");
