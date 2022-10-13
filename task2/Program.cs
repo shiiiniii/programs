95e5fdc17c419b36960351538cc6d56496058033
Anastasia <nastya-shishkina-1999@mail.ru>
2022-10-13 17:41:37 +0300

    add task2

---
+++ task2/Program.cs
@@ -0,0 +1,12 @@
+﻿//Умножить на -1 и двигаться от 1 числа ко 2 
+Console.Clear();
+Console.WriteLine("Вывод чисел от отрицательного до положительного");
+Console.WriteLine("Введите отрицательное число ");
+int number = int.Parse(Console.ReadLine());
+int nbr = number * -1;
+while (number <= nbr) 
+{
+    Console.Write($"{number}, ");
+    number++;
+}
+ 
